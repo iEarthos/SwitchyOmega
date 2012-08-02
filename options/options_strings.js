e6c8c0579e55eb8f1900b8fe5a6a33be3d94d49d
FelisCatus <felis.catus@example.com>
2012-08-02 16:30:00 +0800

    Add a copy of GNU GPL v3 and license blocks. List copyright holders in AUTHORS.

--- options/options_strings.js
+++ options/options_strings.js
@@ -1,1 +1,21 @@
+/*!
+ * Copyright (C) 2012, The SwitchyOmega Authors. Please see the AUTHORS file
+ * for details.
+ *
+ * This file is part of SwitchyOmega.
+ *
+ * SwitchyOmega is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SwitchyOmega is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SwitchyOmega.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 var strings = ["appNameShort", "dialog_close", "dialog_save", "options_title", "options_navHeader_setting", "options_navHeader_profiles", "options_tab_ui", "options_tab_general", "options_tab_importExport", "options_addProfile", "options_group_miscOptions", "options_confirmDelection", "options_refreshOnProfileChange", "options_group_switchOptions", "options_startupProfile", "options_quickSwitch", "options_cycledProfiles", "options_cycledProfilesHelp", "options_notCycledProfiles", "options_group_proxyChanges", "options_revertProxyChanges", "options_group_importExportProfile", "options_exportPacFile", "options_exportPacFileHelp", "options_group_importExportSettings", "options_makeBackup", "options_makeBackupHelp", "options_restoreLocal", "options_restoreLocalHelp", "options_restoreOnline", "options_restoreOnlinePlaceholder", "options_restoreOnlineSubmit", "options_profileTabPrefix", "options_renameProfile", "options_deleteProfile", "options_modalHeader_renameProfile", "options_modalHeader_deleteProfile", "hello", "bye"];
--- scripts/i18n_template2.js
+++ scripts/i18n_template2.js
@@ -1,4 +1,23 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+/*!
+ * Copyright (C) 2012, The SwitchyOmega Authors. Please see the AUTHORS file
+ * for details.
+ * Copyright (c) 2012 The Chromium Authors. All rights reserved.
+ *
+ * This file is part of SwitchyOmega.
+ *
+ * SwitchyOmega is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SwitchyOmega is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SwitchyOmega.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /**
  * @fileoverview This is a simple template engine inspired by JsTemplates
