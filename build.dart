05541ed12e567244cb52c8a858be6ead38a09246
FelisCatus <felis.catus@example.com>
2013-01-13 17:59:40 +0800

    Ignore dwc output and all packages directory.

--- build.dart
+++ build.dart
@@ -2,5 +2,5 @@ import 'package:web_ui/component_build.dart';
 import 'dart:io';
 
 void main() {
-    build(new Options().arguments, []);
+    build(new Options().arguments, ["web/options/options.html"]);
 }
